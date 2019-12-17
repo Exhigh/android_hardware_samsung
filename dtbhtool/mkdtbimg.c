35b0c7b0936fda6255d3f5be0729990f390982e5
ExHigh <exhigh01@gmail.com>
2019-12-17 19:18:46 +0400

    Zero: Fix an issue where dtbhtool crashes with empty arguments

--- dtbhtool/mkdtbimg.c
+++ dtbhtool/mkdtbimg.c
@@ -82,7 +82,9 @@ int main(int argc, char **argv)
             pagesize = strtol(val, NULL, 10);
             if (pagesize == 0)
                 pagesize = default_pagesize;
-        } else {
+        } else if (!strcmp(arg, "")) {
+            // Ignore null argument 
+         } else {
             // Check if this is the dtb path
             int err = stat(arg, &sbuf);
             if (err != 0 || !S_ISDIR(sbuf.st_mode))
